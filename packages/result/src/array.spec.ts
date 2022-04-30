1ef08ed71c5f8dfd24bd520ede987b7f312b34a5
Victor Menge <vfmenge@gmail.com>
2022-04-30 10:22:40 +0200

    feat(result): Array - tranposeAsyncResult, collectAsyncResult, partitionResults and partitionAsyncResults

--- packages/result/src/array.spec.ts
+++ packages/result/src/array.spec.ts
@@ -1,9 +1,18 @@
 import "./array";
 
-import { Err, Ok } from "./result";
+import { Async, AsyncErr, AsyncOk, AsyncResult } from "./asyncResult";
+import { Err, Ok, Result } from "./result";
 
 describe("Array", () => {
-    const isEven = (n: number) => (n % 2 === 0 ? Ok(n) : Err("not even"));
+    const isEven = (n: number): Result<number, string> =>
+        n % 2 === 0 ? Ok(n) : Err("not even");
+
+    const isEvenAsync = (n: number): AsyncResult<number, string> =>
+        n % 2 === 0 ? AsyncOk(n) : AsyncErr("not even");
+
+    const isEvenPromise = (n: number): Promise<Result<number, string>> =>
+        n % 2 === 0 ? Async(Ok(n)) : Async(Err("not even"));
+
     describe(".collectResult", () => {
         it("Given a Result returning function, executes it against all values in the array, returning a Result with all the Ok values if they are all Ok.", () => {
             const actual = [2, 4, 6, 8].collectResult(isEven);
@@ -13,6 +22,30 @@ describe("Array", () => {
 
         it("Given a Result returning function, executes it against all values in the array, returning a Result with the first Err if any returns an Err.", () => {
             const actual = [1, 2, 3, 4].collectResult(isEven);
+
+            expect(actual.err).toEqual("not even");
+        });
+    });
+
+    describe(".collectAsyncResult", () => {
+        it("Given an AsyncResult returning function, executes it against all values in the array, returning a AsyncResult with all the Ok values if they are all Ok.", async () => {
+            const a = await [2, 4, 6, 8].collectAsyncResult(isEvenAsync);
+            expect(a.val).toEqual([2, 4, 6, 8]);
+        });
+
+        it("Given an Promise<Result> returning function, executes it against all values in the array, returning a AsyncResult with all the Ok values if they are all Ok.", async () => {
+            const b = await [2, 4, 6, 8].collectAsyncResult(isEvenPromise);
+            expect(b.val).toEqual([2, 4, 6, 8]);
+        });
+
+        it("Given an AsyncResult returning function, executes it against all values in the array, returning a AsyncResult with the first Err if any returns an Err.", async () => {
+            const actual = await [1, 2, 3, 4].collectAsyncResult(isEvenAsync);
+
+            expect(actual.err).toEqual("not even");
+        });
+
+        it("Given an PromiseResult returning function, executes it against all values in the array, returning a AsyncResult with the first Err if any returns an Err.", async () => {
+            const actual = await [1, 2, 3, 4].collectAsyncResult(isEvenPromise);
 
             expect(actual.err).toEqual("not even");
         });
@@ -29,6 +62,36 @@ describe("Array", () => {
         it("Given a Result returning function, executes it against all values in the array, returning a Result with the first Err if any returns an Err.", () => {
             const foo = [1, 2, 3, 4].map(isEven);
             const actual = Array.transposeResult(foo);
+
+            expect(actual.err).toEqual("not even");
+        });
+    });
+
+    describe("::transposeAsyncResult", () => {
+        it("Given a AsyncResult returning function, executes it against all values in the array, returning a AsyncResult with all the Ok values if they are all Ok.", async () => {
+            const foo = [2, 4, 6, 8].map(isEvenAsync);
+            const actual = await Array.transposeAsyncResult(foo);
+
+            expect(actual.val).toEqual([2, 4, 6, 8]);
+        });
+
+        it("Given a Promise<Result> returning function, executes it against all values in the array, returning a AsyncResult with all the Ok values if they are all Ok.", async () => {
+            const foo = [2, 4, 6, 8].map(isEvenPromise);
+            const actual = await Array.transposeAsyncResult(foo);
+
+            expect(actual.val).toEqual([2, 4, 6, 8]);
+        });
+
+        it("Given a AsyncResult returning function, executes it against all values in the array, returning a AsyncResult with the first Err if any returns an Err.", async () => {
+            const foo = [1, 2, 3, 4].map(isEvenAsync);
+            const actual = await Array.transposeAsyncResult(foo);
+
+            expect(actual.err).toEqual("not even");
+        });
+
+        it("Given a Promise<Result> returning function, executes it against all values in the array, returning a AsyncResult with the first Err if any returns an Err.", async () => {
+            const foo = [1, 2, 3, 4].map(isEvenPromise);
+            const actual = await Array.transposeAsyncResult(foo);
 
             expect(actual.err).toEqual("not even");
         });
--- packages/result/src/array.ts
+++ packages/result/src/array.ts
@@ -1,14 +1,198 @@
+import { AsyncResult } from "./asyncResult";
 import { Err, Ok, Result } from "./result";
 
 declare global {
     interface Array<T> {
+        /**
+         * `this: Array<T>`
+         *
+         * `collectResult: (T -> Result<A, E>) -> Result<Array<A>, E>`
+
+         * ---
+         * @example
+         * const isEven = 
+         *   (x: number): Result<number, string> => x % 2 === 0 ? Ok(x) : Err(`${x}: not even`);
+         * 
+         * const a = [2, 4, 6, 8].collectResult(isEven);
+         * expect(a).toBeInstanceOf(Result);
+         * expect(a.unwrap()).toEqual([2, 4, 6, 8])
+         * 
+         * const b = [1, 2, 3, 4].collectResult(isEven);
+         * expect(b).toBeInstanceOf(Result);
+         * expect(b.unwrapErr()).toEqual("1: not even");
+         */
         collectResult<A, E>(fn: (t: T) => Result<A, E>): Result<Array<A>, E>;
+
+        /**
+         * `this: Array<T>`
+         *
+         * `collectAsyncResult: (T -> AsyncResult<A, E>) -> AsyncResult<Array<A>, E>`
+
+         * ---
+         * @example
+         * declare function parseNumAsync(x: string): AsyncResult<number, string>;
+         * 
+         * const a = ["1", "2", "3"].collectAsyncResult(parseNumAsync);
+         * expect(a).toBeInstanceOf(AsyncResult);
+         * expect((await a.unwrap())).toEqual([1, 2, 3])
+         * 
+         * const b = ["1", "bla", "2", "ble"].collectAsyncResult(parseNumAsync);
+         * expect(b).toBeInstanceOf(AsyncResult);
+         * expect((await b.unwrapErr())).toEqual("bla: not a number");
+         */
+        collectAsyncResult<A, E>(
+            fn: (t: T) => AsyncResult<A, E>
+        ): AsyncResult<Array<A>, E>;
+
+        /**
+         * `this: Array<T>`
+         *
+         * `collectAsyncResult: (T -> Promise<Result<A, E>>) -> Promise<Result<Array<A>, E>>`
+
+         * ---
+         * @example
+         * declare function parseNumAsync(x: string): Promise<Result<number, string>>;
+         * 
+         * const a = ["1", "2", "3"].collectAsyncResult(parseNumAsync);
+         * expect(a).toBeInstanceOf(AsyncResult);
+         * expect((await a.unwrap())).toEqual([1, 2, 3])
+         * 
+         * const b = ["1", "bla", "2", "ble"].collectAsyncResult(parseNumAsync);
+         * expect(b).toBeInstanceOf(AsyncResult);
+         * expect((await b.unwrapErr())).toEqual("bla: not a number");
+         */
+        collectAsyncResult<A, E>(
+            fn: (t: T) => Promise<Result<A, E>>
+        ): AsyncResult<Array<A>, E>;
     }
 
     interface ArrayConstructor {
+        /**
+         * `tranposeResult: Array<Result<A, E>> -> Result<Array<A>, E>`
+         *
+         * ---
+         * Iterates over the `Array`, stopping as soon as a `Result` that is an `Err` is found.
+         * @returns a `Result` with an `Array` with all the `Ok` values if no `Err` is found, otherwise returns that `Err`.
+         * @example
+         * declare function parseNum(x: string): Result<number, string>;
+         *
+         * const a: Array<Result<number, string>> = ["1", "2", "3"].map(parseNum);
+         * const b: Result<Array<A>, E> = Array.tranposeResult(a);
+         *
+         * expect(b).toBeInstanceOf(AsyncResult);
+         * expect(b.unwrap()).toEqual([1, 2, 3])
+         *
+         * const c = ["1", "bla", "2", "ble"].map(parseNum);
+         * const d = Array.tranposeResult(c);
+         *
+         * expect(d).toBeInstanceOf(Result);
+         * expect(d.unwrapErr()).toEqual("bla: not a number");
+         */
         transposeResult<A, E>(
             results: Array<Result<A, E>>
         ): Result<Array<A>, E>;
+
+        /**
+         * `transposeAsyncResult: Array<AsyncResult<A, E>> -> AsyncResult<Array<A>, E>`
+         *
+         * ---
+         * Iterates over the `Array`, stopping as soon as a `AsyncResult` that is an `Err` is found.
+         * @returns a `AsyncResult` with an `Array` with all the `Ok` values if no `Err` is found, otherwise returns that `Err`.
+         * @example
+         * declare function parseNumAsync(x: string): AsyncResult<number, string>;
+         *
+         * const a: Array<AsyncResult<number, string>> = ["1", "2", "3"].map(parseNumAsync);
+         * const b: AsyncResult<Array<A>, E> = Array.tranposeAsyncResult(a);
+         *
+         * expect(b).toBeInstanceOf(AsyncResult);
+         * expect((await b.unwrap())).toEqual([1, 2, 3])
+         *
+         * const c = ["1", "bla", "2", "ble"].map(parseNumAsync);
+         * const d = Array.tranposeAsyncResult(c);
+         *
+         * expect(d).toBeInstanceOf(AsyncResult);
+         * expect((await d.unwrapErr())).toEqual("bla: not a number");
+         */
+        transposeAsyncResult<A, E>(
+            results: Array<AsyncResult<A, E>>
+        ): AsyncResult<Array<A>, E>;
+
+        /**
+         * `transposeAsyncResult: Array<Promise<Result<A, E>>> -> AsyncResult<Array<A>, E>`
+         *
+         * ---
+         * Iterates over the `Array`, stopping as soon as a `Promise` that contains an `Err` `Result` is found.
+         * @returns a `AsyncResult` with an `Array` with all the `Ok` values if no `Err` is found, otherwise returns that `Err`.
+         * @example
+         * declare function parseNumAsync(x: string): Promise<Result<number, string>>;
+         *
+         * const a: Array<Promise<Result<number, string>>> = ["1", "2", "3"].map(parseNumAsync);
+         * const b: AsyncResult<Array<A>, E> = Array.tranposeAsyncResult(a);
+         *
+         * expect(b).toBeInstanceOf(AsyncResult);
+         * expect((await b.unwrap())).toEqual([1, 2, 3])
+         *
+         * const c = ["1", "bla", "2", "ble"].map(parseNumAsync);
+         * const d = Array.tranposeAsyncResult(c);
+         *
+         * expect(d).toBeInstanceOf(AsyncResult);
+         * expect((await d.unwrapErr())).toEqual("bla: not a number");
+         */
+        transposeAsyncResult<A, E>(
+            results: Array<Promise<Result<A, E>>>
+        ): AsyncResult<Array<A>, E>;
+
+        /**
+         * `partitionResults: Array<Result<A, E>> -> Array<A> * Array<E>`
+         *
+         * ---
+         * Paritions an Array of Results into an Array with the extracted Oks tupled with an Array with the extracted Errs.
+         * @example
+         * const arr = [Ok(1), Ok(2), Err("oops")];
+         * const [oks, errs] = Array.partitionResults(arr);
+         *
+         * expect(oks).toEqual([1, 2])
+         * expect(errs).toEqual(["oops"]);
+         */
+        partitionResults<A, E>(
+            results: Array<Result<A, E>>
+        ): [Array<A>, Array<E>];
+
+        /**
+         * `partitionAsyncResults: Array<AsyncResult<A, E>> -> Promise<Array<A> * Array<E>>`
+         *
+         * ---
+         * Paritions an Array of AsyncResults into an Array with the extracted Oks tupled with an Array with the extracted Errs.
+         * @example
+         * const arr = [AsyncOk(1), AsyncOk(2), AsyncErr("oops")];
+         * const [oks, errs] = await Array.partitionAsyncResults(arr);
+         *
+         * expect(oks).toEqual([1, 2])
+         * expect(errs).toEqual(["oops"]);
+         */
+        partitionAsyncResults<A, E>(
+            results: Array<AsyncResult<A, E>>
+        ): Promise<[Array<A>, Array<E>]>;
+
+        /**
+         * `partitionAsyncResults: Array<Promise<Result<A, E>>> -> Promise<Array<A> * Array<E>>`
+         *
+         * ---
+         * Paritions an Array of Promise<Result>s into an Array with the extracted Oks tupled with an Array with the extracted Errs.
+         * @example
+         * const arr = [
+         *   Promise.resolve(Ok(1)),
+         *   Promise.resolve(Ok(2)),
+         *   Promise.resolve(Err("oops"))
+         * ];
+         * const [oks, errs] = await Array.partitionAsyncResults(arr);
+         *
+         * expect(oks).toEqual([1, 2])
+         * expect(errs).toEqual(["oops"]);
+         */
+        partitionAsyncResults<A, E>(
+            results: Array<Promise<Result<A, E>>>
+        ): Promise<[Array<A>, Array<E>]>;
     }
 }
 
@@ -36,5 +220,72 @@ Object.defineProperty(Array, "transposeResult", {
     enumerable: false,
     value: function <A, E>(results: Array<Result<A, E>>): Result<Array<A>, E> {
         return results.collectResult(x => x);
+    },
+});
+
+Object.defineProperty(Array.prototype, "collectAsyncResult", {
+    enumerable: false,
+    value: function <T, A, E>(
+        this: Array<T>,
+        fn: (t: T) => AsyncResult<A, E> | Promise<Result<A, E>>
+    ): AsyncResult<Array<A>, E> {
+        const prom = Promise.resolve(async () => {
+            let res = [];
+
+            for (const el of this) {
+                const x = await fn(el);
+
+                if (x.isErr()) return Err(x.err!);
+
+                res.push(x.val!);
+            }
+
+            return Ok(res);
+        }).then(x => x());
+
+        return AsyncResult.from(prom as any);
+    },
+});
+
+Object.defineProperty(Array, "transposeAsyncResult", {
+    enumerable: false,
+    value: function <A, E>(
+        results: Array<AsyncResult<A, E> | Promise<Result<A, E>>>
+    ): AsyncResult<Array<A>, E> {
+        return results.collectAsyncResult(x => x as any);
+    },
+});
+
+Object.defineProperty(Array, "partitionResults", {
+    enumerable: false,
+    value: function <A, E>(results: Array<Result<A, E>>): [Array<A>, Array<E>] {
+        let oks = [];
+        let errs = [];
+
+        for (const r of results) {
+            if (r.isOk()) oks.push(r.val);
+            else errs.push(r.err);
+        }
+
+        return [oks, errs] as any;
+    },
+});
+
+Object.defineProperty(Array, "partitionAsyncResults", {
+    enumerable: false,
+    value: async function <A, E>(
+        results: Array<AsyncResult<A, E> | Promise<Result<A, E>>>
+    ): Promise<[Array<A>, Array<E>]> {
+        let oks = [];
+        let errs = [];
+
+        const rs = await Promise.all(results);
+
+        for (const r of rs) {
+            if (r.isOk()) oks.push(r.val);
+            else errs.push(r.err);
+        }
+
+        return [oks, errs] as any;
     },
 });
